40b70a85dbeb80efbcd105d9896a6d53ccc47773
Ramon Melo <ramonmelo@dunk.(none)>
2014-08-21 22:06:06 -0300

    Init goalie agent in its right position

--- smsoccer/players/goalie_agent.py
+++ smsoccer/players/goalie_agent.py
@@ -16,4 +16,10 @@ class GoalieAgent(AbstractAgent):
         """
         Think method
         """
-        pass
+        if not self.in_kick_off_formation:
+            position_point = player_position(self.wm.uniform_number, self.wm.side == WorldModel.SIDE_R)
+            # Teleport to right position
+            self.wm.teleport_to_point(position_point)
+            # Player is ready in formation
+            self.in_kick_off_formation = True
+            return
