2d8efff5ee49cd541d8bb4de4f5e213b93bf7a9b
dav <fds>
2014-08-23 23:01:54 -0300

    particle filter for localization

--- smsoccer/localization/particlefilter.py
+++ smsoccer/localization/particlefilter.py
@@ -1,10 +1,22 @@
 import math
+from numpy.random import random
+
+from smsoccer.localization.distributions import multivariate_normal
+
 
 __author__ = 'dav'
 import numpy as np
 
 # Number of particles
 N = 10
+
+# Motion variance (theta is zero)
+VAR_X = 3
+VAR_Y = 0.0
+VAR_T = 0.0
+
+# Update variance: x, y, theta.
+sigma = np.array([[1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 0.1]])
 
 
 def _update_estimated_position():
@@ -29,23 +41,22 @@ class ParticleFilter(object):
         :param dash: (vel_lin, vel_ang)
         """
         theta = self.e_position[2]
+        #TODO compute c
         c = 0.01  # convert dash to velocity
         dx = dash * c * math.cos(math.radians(theta))
         dy = dash * c * math.sin(math.radians(theta))
 
-        varx = 0.1
-        vary = 0.1
 
         # random movement
-        rdx = varx * np.random.randn(N) + dx
-        rdy = vary * np.random.randn(N) + dy
+        rdx = VAR_X * np.random.randn(N) + dx
+        rdy = VAR_Y * np.random.randn(N) + dy
+        rdth = VAR_T * np.random.randn(N) + theta
 
         #movement in x,y
         mov = np.vstack((rdx, rdy))
         #movement with theta=0
-        mov = np.vstack((mov, np.zeros(N)))
+        mov = np.vstack((mov, rdth))
 
-        print mov
         self.particles = self.particles + mov.T
 
         self._update_estimated_position()
@@ -58,23 +69,55 @@ class ParticleFilter(object):
         self.particles[:, 2] += angle
         self._update_estimated_position()
 
+    def resample(self, weights):
+
+        indices = []
+        c = [0.] + [sum(weights[:i + 1]) for i in range(N)]
+        u0, j = random(), 0
+        for u in [(u0 + i) / N for i in range(N)]:
+            while u > c[j]:
+                j += 1
+            indices.append(j - 1)
+        return indices
+
+
+    def update_particles(self, perception):
+
+        # Weights.
+        w = []
+        for x in self.particles:
+            w1 = multivariate_normal(x, perception, sigma)
+            w.append(w1)
+
+
+        # normalize the weights.
+        w = np.array(w) / sum(w)
+        print w
+        # Resample
+        ids = self.resample(w)
+        print ids
+        self.particles = self.particles[ids]
+
 
     def _update_estimated_position(self):
         self.e_position = np.mean(self.particles, axis=0)
 
 
 if __name__ == "__main__":
-    pf = ParticleFilter((5, 10, 0))
-    print pf.particles
+    pf = ParticleFilter([5, 10, 0])
+    # print pf.particles
 
-    # dash
+    # # dash
     pf.dash_particles(10)
     print pf.particles, pf.e_position
+    #
+    # #rotate
+    # pf.rotate_particles(30)
+    # print pf.particles, pf.e_position
 
-    #rotate
-    pf.rotate_particles(30)
+    pf.update_particles([5, 10, 0])
+
     print pf.particles, pf.e_position
-
 
 
 
